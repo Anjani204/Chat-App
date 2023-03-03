f17e222e81bf529dd17720b2d95600687f296476
Anjani Haripriya <anjaniharipriyaparsa2141@gmail.com>
2023-03-03 16:17:06 +0530

    anju committed

--- js/client.js
+++ js/client.js
@@ -1,4 +1,5 @@
 const socket = io('http://localhost:8000',{transports:['websocket']})
+// const socket = io('http://localhost:8000')
 
 const form=document.getElementById('send-container')
 const messageInp=document.getElementById('messageInp')
